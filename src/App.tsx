98a26d3836f16989cd01446db0e4b7a3d9f72946
SumanMarahathaDH <smarahatta@deerhold.com>
2023-01-20 21:58:57 +0545

    added linters/preetier && pre-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-const App:React.FC = () => {
-  return (
-    <div>Welcome to React Typescript boilerplate</div>
-  )
+const App: React.FC = () => {
+  return <div>Welcome to React Typescript boilerplate</div>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './index.css';
+import './index.css'
 import 'core-js/features/array/flat-map'
 import 'core-js/features/map'
 import 'core-js/features/promise'
@@ -9,6 +9,6 @@ import 'core-js/features/set'
 import 'raf/polyfill'
 import 'whatwg-fetch'
 
-const container = document.getElementById('root')!
+const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<App />)
