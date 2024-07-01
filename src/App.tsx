35579aebce6d5cd0e7d03965bee351dbe08e9ae2
nyetes <nitesh.mahar1208@gmail.com>
2024-07-01 21:42:54 +0545

    rem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './App.css';
+import ReactDom from 'react-dom';
+import Blog from './pages/Blog';
+import Home from './pages/Home'
+import { Route } from 'react-router-dom';
+
+
 
 const App: React.FC = () => {
-  return <div>Welcome to React Typescript boilerplate</div>
-}
+  return (
+  <div className ='container'>
+    <Route path="/" Component={Home} />
+    <Route path="/blog/:id" Component={Blog} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import * as React from 'react'
+// import { createRoot } from 'react-dom/client'
+import ReactDOM, * as ReactDom from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App'
 import './index.css'
 import 'core-js/features/array/flat-map'
@@ -9,6 +11,16 @@ import 'core-js/features/set'
 import 'raf/polyfill'
 import 'whatwg-fetch'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+// const container = document.getElementById('root')
+// const root = createRoot(container)
+// root.render(<App />)
+// 
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
